d4fc56b9860cacde703356c943bdaa3cdb8048c5
kapil gupta <kgupta@sangoma.com>
2025-02-19 00:32:23 -0500

    FREEI-1733 Follow me issue with external number + sign

--- Dialplan/macroDial.php
+++ Dialplan/macroDial.php
@@ -69,6 +69,9 @@ class macroDial{
 	/****************************************************/
 	$ext->add($c,$s,'', new \ext_execif('$["${FMFM}" = "TRUE"]','Set','RGFMDIAL=${EXTTOCALL}','Set','RGFMDIAL=${NODEST}'));
 	$ext->add($c,$s, 'nddialapp', new \ext_gosub(1,'${RGFMDIAL}','dial-with-exten'));
+	/* dialplan for + */
+	$ext->add('dial-with-exten', '_+X.', '', new \ext_dial('${ds}${CWRING}b(func-apply-sipheaders^s^1)', ''));
+        $ext->add('dial-with-exten', '_+X.','', new \ext_return());
 	$ext->add('dial-with-exten', '_X.', '', new \ext_dial('${ds}${CWRING}b(func-apply-sipheaders^s^1)', ''));
         $ext->add('dial-with-exten','_X.','', new \ext_return());
 	$ext->add('dial-with-exten', '_X', '', new \ext_dial('${ds}${CWRING}b(func-apply-sipheaders^s^1)', ''));
