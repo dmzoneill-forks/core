bb6e6880f6783aab16d21848dad874e4ffbf39a8
Rahul Verma <rverma@sangoma.com>
2025-02-24 05:04:02 -0500

    FREEI-1637 With asterisk 18.20.2, MES graph is not displayed in cdrpro

--- Dialplan/macroDial.php
+++ Dialplan/macroDial.php
@@ -76,6 +76,7 @@ class macroDial{
         $ext->add('dial-with-exten','_X.','', new \ext_return());
 	$ext->add('dial-with-exten', '_X', '', new \ext_dial('${ds}${CWRING}b(func-apply-sipheaders^s^1)', ''));
         $ext->add('dial-with-exten','_X','', new \ext_return());
+        $ext->add('dial-with-exten', 'h', '', new \ext_macro('hangupcall'));
 	/******************************************************/
         $ext->add($c,$s,'', new \ext_set('DIALSTATUS', '${IF($["${DIALSTATUS_CW}"!="" ]?${DIALSTATUS_CW}:${DIALSTATUS})}'));
         $ext->add($c,$s,'', new \ext_gosubif('$[("${SCREEN}" != "" & ("${DIALSTATUS}" = "TORTURE" | "${DIALSTATUS}" = "DONTCALL"))  | "${DIALSTATUS}" = "ANSWER"]', '${DIALSTATUS},1'));
@@ -114,6 +115,7 @@ class macroDial{
 	$ext->add($c,$s, 'hsdialapp', new \ext_gosub(1,'${RGFMDIAL}','dial-ext-with-exten'));
 	$ext->add('dial-ext-with-exten', '_[+-X].','', new \ext_dial('${${HuntMember}}${ds}${CWRING}b(func-apply-sipheaders^s^1)', ''));
 	$ext->add('dial-ext-with-exten', '_[+-X].','', new \ext_return());
+        $ext->add('dial-ext-with-exten', 'h', '', new \ext_macro('hangupcall'));
 	/*******************************************************************/
 	$ext->add($c,$s,'', new \ext_gotoif('$["${DIALSTATUS}" = "ANSWER"]', 'ANSWER,1'));
         $ext->add($c,$s,'', new \ext_set('HuntLoop', '$[1 + ${HuntLoop}]'));
