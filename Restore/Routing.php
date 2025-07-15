af3875409e4aff745768cb46a9977b27f0a5e971
kapil gupta <kgupta@sangoma.com>
2025-07-15 10:16:08 -0400

    Fixing FREEI-2129

--- Restore/Routing.php
+++ Restore/Routing.php
@@ -9,10 +9,10 @@ class Routing extends Corebase{
 			// get notif
 			 $emailfrom = $emailto =  $emailsubject =  $emailbody = '';
 			if(isset($route['notification'])){
-				$notification = $route['notification']['emailfrom'];
-				$emailto= $route['notification']['emailto'];
-				$emailsubject=$route['notification']['emailsubject'];
-				$emailbody= $route['notification']['emailbody'];
+				$notification = $route['notification']['emailfrom'] ?? '';
+				$emailto= $route['notification']['emailto'] ?? '';
+				$emailsubject=$route['notification']['emailsubject'] ?? '';
+				$emailbody= $route['notification']['emailbody'] ?? '';
 			}
 			$routing->editById($route['route_id'], $route['name'], $route['outcid'], $route['outcid_mode'], $route['password'], $route['emergency_route'], $route['intracompany_route'], $route['mohclass'], $route['time_group_id'], $route['patterns'], $route['trunks'], $route['seq'], $route['dest'], $route['time_mode'], $route['timezone'], $route['calendar_id'], $route['calendar_group_id'], $route['notification_on'], $emailfrom , $emailto, $emailsubject, $emailbody);
 		}
