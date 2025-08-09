d5a8a01b70d4b9f4ea2c354ae5447dd0af016c8f
Javier Pastor <jpastor@cerebelum.net>
2025-08-09 20:57:33 +0200

    Issues #168
    Fix check if notification is array
    Fix assignation var "emailfrom"

--- Restore/Routing.php
+++ Restore/Routing.php
@@ -7,12 +7,12 @@ class Routing extends Corebase{
 		$routing = new \FreePBX\modules\Core\Components\Outboundrouting($this->FreePBX->Database);
 		foreach ($configs as $route) {
 			// get notif
-			 $emailfrom = $emailto =  $emailsubject =  $emailbody = '';
-			if(isset($route['notification'])){
-				$notification = $route['notification']['emailfrom'] ?? '';
-				$emailto= $route['notification']['emailto'] ?? '';
-				$emailsubject=$route['notification']['emailsubject'] ?? '';
-				$emailbody= $route['notification']['emailbody'] ?? '';
+			$emailfrom = $emailto =  $emailsubject =  $emailbody = '';
+			if(isset($route['notification']) && is_array($route['notification'])) {
+				$emailfrom    = $route['notification']['emailfrom'] ?? '';
+				$emailto      = $route['notification']['emailto'] ?? '';
+				$emailsubject = $route['notification']['emailsubject'] ?? '';
+				$emailbody    = $route['notification']['emailbody'] ?? '';
 			}
 			$routing->editById($route['route_id'], $route['name'], $route['outcid'], $route['outcid_mode'], $route['password'], $route['emergency_route'], $route['intracompany_route'], $route['mohclass'], $route['time_group_id'], $route['patterns'], $route['trunks'], $route['seq'], $route['dest'], $route['time_mode'], $route['timezone'], $route['calendar_id'], $route['calendar_group_id'], $route['notification_on'], $emailfrom , $emailto, $emailsubject, $emailbody);
 		}
